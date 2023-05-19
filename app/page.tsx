dc62aa2940efa30f6f264d40b3b9dd2117538bd1
alberpeb <alber.perezb@gmail.com>
2023-05-19 20:19:08 -0300

    WB-2: creating a basic landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
-export default async function HomePage() {
+const Navbar = () => {
   return (
-    <div className="flex items-center flex-col mx-auto w-full justify-center mt-16 px-8">
-      hello there!
+    <nav>
+      <a href="/login">login</a>
+    </nav>
+  );
+};
+
+const Body = () => {
+  return (
+    <div>
+      <h1>Your All-in-One Social Hub</h1>
+      <p>
+      Our platform simplifies and enhances your online presence by consolidating all your social media links in one convenient place. Whether you're an influencer, entrepreneur, or simply want to share your digital footprint with the world, Wisp.bio is here to help
+      </p>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright &copy; 2023</p>
+    </footer>
+  );
+};
+
+export default async function Landing() {
+  return (
+    <div>
+      <Navbar />
+      <Body />
+      <Footer />
     </div>
   );
 }
