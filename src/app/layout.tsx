cdb9c1c625042080583f4696ceb873e50e441d17
alberpeb <alber.perezb@gmail.com>
2023-07-15 20:55:50 -0300

    WB-37: provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import '@/styles/globals.css';
+import { NextAuthProvider } from 'src/nextAuthProvider';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <NextAuthProvider>
+        <body>{children}</body>
+      </NextAuthProvider>
     </html>
   );
 }
